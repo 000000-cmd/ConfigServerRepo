72b89bcee4fe2082543387e1a08f6b7542ae50f0
Jeisson Fernando Leon Avila <jeissonfernandoleonavila@gmail.com>
2025-11-22 20:17:57 -0500

    ...

---
+++ src/test/java/com/saasbeauty/infra/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saasbeauty.infra.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
